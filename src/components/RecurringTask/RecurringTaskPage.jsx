9b1d4d9fadc91e16e7d30bc96023034734cd5555
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-06 21:56:00 +0530

    change in recurringTaskPage component

---
+++ src/components/RecurringTask/RecurringTaskPage.jsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import EvaluateProgressPage from "./EvaluateProgressPage";
+import CategorySelectionPage from "./CategorySelectionPage";
+import DefineTaskPage from "./DefineTaskPage";
+import HowOftenPage from "./HowOftenPage";
+import WhenToDoItPage from "./WhenToDoItPage";
+import axiosInstance from "@/services/axiosInstance";
+
+function RecurringTaskPage() {
+  const [step, setStep] = useState(0);
+
+  const [component1Value, setComponent1Value] = useState(null);
+  const [component2Value, setComponent2Value] = useState(null);
+  const [component3Value, setComponent3Value] = useState(null);
+  const [component4Value, setComponent4Value] = useState(null);
+  const [component5Value, setComponent5Value] = useState(null);
+
+  const finalArray = [
+    component1Value,
+    component2Value,
+    component3Value,
+    component4Value,
+    component5Value,
+  ];
+
+  const router = useRouter();
+
+  const nextStep = () => setStep((prevStep) => prevStep + 1);
+  const previousStep = () => setStep((prevStep) => (prevStep > 0 ? prevStep - 1 : 0));
+
+  const handleSave = async (savedData) => {
+    try {
+      const response = await axiosInstance.post(
+        "/recurringTask/recurring-task",
+        savedData
+      );
+      console.log("Saved form data:", response);
+
+      alert("Task saved successfully!");
+      router.push("/");
+    } catch (err) {
+      console.error("Error saving task", err);
+      alert("Failed to save the task. Please try again.");
+    }
+  };
+
+  return (
+    <div>
+      {step === 0 && (
+        <CategorySelectionPage
+          onNext={nextStep}
+          setValue={setComponent1Value}
+        />
+      )}
+      {step === 1 && (
+        <EvaluateProgressPage
+          onNext={nextStep}
+          setValue={setComponent2Value}
+          onPrevious={previousStep}
+        />
+      )}
+      {step === 2 && (
+        <DefineTaskPage
+          onNext={nextStep}
+          setValue={setComponent3Value}
+          onPrevious={previousStep}
+        />
+      )}
+      {step === 3 && (
+        <HowOftenPage
+          onNext={nextStep}
+          setValue={setComponent4Value}
+          onPrevious={previousStep}
+        />
+      )}
+      {step === 4 && (
+        <WhenToDoItPage
+          onSave={handleSave}
+          setValue={setComponent5Value}
+          onPrevious={previousStep}
+        />
+      )}
+    </div>
+  );
+}
+
+export default RecurringTaskPage;
