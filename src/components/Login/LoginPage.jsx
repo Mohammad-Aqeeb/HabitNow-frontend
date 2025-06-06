8c2944f18b206e62c4e12de7698a9e73e400bd15
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-06 21:55:01 +0530

    change in loginPage component

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import styles from '../../styles/login.module.css';
+import styles from '@/styles/login.module.css';
 import axiosInstance from '@/services/axiosInstance';
 
 const LoginPage = () => {
