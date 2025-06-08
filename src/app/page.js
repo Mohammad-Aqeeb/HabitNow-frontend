80deb2d9661773614a410283dfc1c0f5ddd9dba6
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-08 23:32:38 +0530

    change in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import dynamic from 'next/dynamic';
 import { motion } from 'framer-motion';
+import ProtectedRoute from '@/components/ProtectedRoute';
 
 const HomePage = dynamic(() => import('../components/Home/HomePage'), {
   ssr: false,
@@ -27,7 +28,9 @@ export default function Home() {
         alignItems: 'center',
       }}
     >
-      <HomePage />
+      <ProtectedRoute>
+        <HomePage />
+      </ProtectedRoute>
     </motion.div>
   );
 }
