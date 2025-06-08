644fed85519b0830f2c0a58abf32325dc71bcee5
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-08 23:32:27 +0530

    change in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import { motion, AnimatePresence } from 'framer-motion';
 import Sidenav from '../components/Sidenav/Sidenav';
 import BottomNavbarPage from '../components/BottomNavbar/BottomNavbarPage';
 import './globals.css';
+import { TaskProvider } from '@/context/TaskProvider';
+import { AuthProvider } from '@/context/AuthContext';
+import { FormProvider } from '@/context/FormContext';
 
 const pageVariants = {
   initial: { opacity: 0, y: 30 },
@@ -14,27 +17,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="MainDiv" style={{ position: 'relative' }}>
-          <Sidenav/>
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={typeof window !== 'undefined' ? location.pathname : 'static'}
-              variants={pageVariants}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              style={{
-                display: 'flex',
-                width: '100%',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              {children}
-            </motion.div>
-          </AnimatePresence>
-          <BottomNavbarPage />
-        </div>
+        <AuthProvider>
+          <TaskProvider>
+            <FormProvider>
+              <div className="MainDiv" style={{ position: 'relative' }}>
+                <Sidenav />
+                <AnimatePresence mode="wait">
+                  <motion.div
+                    key={typeof window !== 'undefined' ? location.pathname : 'static'}
+                    variants={pageVariants}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    style={{
+                      display: 'flex',
+                      width: '100%',
+                      justifyContent: 'center',
+                      alignItems: 'center'
+                    }}
+                  >
+                    {children}
+                  </motion.div>
+                </AnimatePresence>
+                <BottomNavbarPage />
+              </div>
+            </FormProvider>
+          </TaskProvider>
+        </AuthProvider>
       </body>
     </html>
   );
