b00e6a8afe68040997ae7419342938316afdadc0
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-09 23:13:00 +0530

    change in habit page

--- src/app/habit/page.js
+++ src/app/habit/page.js
@@ -3,7 +3,7 @@
 import ProtectedRoute from '@/components/ProtectedRoute';
 import dynamic from 'next/dynamic';
 
-const HabitPage = dynamic(() => import('../../components/Habits/HabitPage'), {
+const HabitPage = dynamic(() => import('@/components/Habits/HabitPage'), {
   ssr: false,
 });
 
