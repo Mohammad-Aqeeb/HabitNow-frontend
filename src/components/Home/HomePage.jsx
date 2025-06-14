eb8db97c4fea5a63454eb101f9a0b2b1d5112a48
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-14 20:38:11 +0530

    change in homePage component

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,51 +1,45 @@
 'use client';
 
 import React, { useState, useEffect, useRef } from 'react';
-import {
-  FaCalendarAlt,
-  FaPlus,
-  FaEye,
-  FaEdit,
-  FaTrashAlt,
-  // FaSearch,
-  // FaQuestionCircle,
-  // FaFilter,
-} from 'react-icons/fa';
-import axiosInstance from '@/services/axiosInstance';
+import { FaCalendarAlt, FaPlus } from 'react-icons/fa';
 import styles from '@/styles/home.module.css';
 import ChooseTaskModal from '../ChooseTaskModal/ChooseTaskModal';
 import Navbar from '../Navbar/Navbar';
 
 import dayjs from 'dayjs';
-import Rate from '../Rate/Rate';
+
+import { useDispatch, useSelector } from 'react-redux';
+import {fetchSingleTasks, toggleTaskCompletion, updateSingleTask } from '@/slices/taskSlice';
+import { fetchRecurringTask, toggleRecurringTaskCompletion, updateRecurringTask } from '@/slices/recurringtaskSlice';
+
 
 const HomePage = () => {
-  const [singleTasks, setSingleTasks] = useState([]);
-  const [recurringTasks, setRecurringTasks] = useState([]);
+  const dispatch = useDispatch();
+  const singleTasks = useSelector((state) => state.tasks.items);
+  const recurringTasks = useSelector((state) => state.recurringtasks.items);
   const [selectedDate, setSelectedDate] = useState(dayjs());
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [editModal, setEditModal] = useState({ isOpen: false, task: null, type: '' });
-  const isRatePage = false;
 
   const startDate = dayjs().subtract(80, 'day');
   const dates = Array.from({ length: 161 }, (_, i) => startDate.add(i, 'day'));
   const dateRefs = useRef([]);
   dateRefs.current = [];
 
+  const handleDateClick = (date) => setSelectedDate(date);
+  const handlePlusClick = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const singleTasksResponse = await axiosInstance.get('/task/tasks');
-        setSingleTasks(singleTasksResponse.data.tasks);
-
-        const recurringTasksResponse = await axiosInstance.get('/recurringTask/recurring-tasks');
-        setRecurringTasks(recurringTasksResponse.data.recurringTasks);
+        await dispatch(fetchSingleTasks()).unwrap();
+        await dispatch(fetchRecurringTask()).unwrap();
       } catch (error) {
         console.error('Error fetching tasks:', error);
       }
     };
     fetchData();
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     const index = dates.findIndex((date) => date.isSame(selectedDate, 'day'));
@@ -54,63 +48,13 @@ const HomePage = () => {
     }
   }, [selectedDate, dates]);
 
-  const handleDateClick = (date) => setSelectedDate(date);
-  const handlePlusClick = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-  const handleEditModalClose = () => setEditModal({ isOpen: false, task: null, type: '' });
-
-  const openEditModal = (task, type) => setEditModal({ isOpen: true, task, type });
-
-  const saveTaskChanges = async () => {
-    try {
-      const { task, type } = editModal;
-      const url = type === 'single' ? `/task/tasks/${task._id}` : `/recurringTask/recurring-task/${task._id}`;
-      await axiosInstance.put(url, task);
-
-      if (type === 'single') {
-        setSingleTasks(singleTasks.map((t) => (t._id === task._id ? task : t)));
-      } else {
-        setRecurringTasks(recurringTasks.map((t) => (t._id === task._id ? task : t)));
-      }
-
-      handleEditModalClose();
-      alert('Task updated successfully.');
-    } catch (error) {
-      console.error('Error updating task:', error);
-      alert('Failed to update task. Please try again.');
-    }
-  };
-
-  const handleDelete = async (id, type) => {
-    try {
-      const url = type === 'single' ? `/task/tasks/${id}` : `/recurringTask/recurring-task/${id}`;
-      await axiosInstance.delete(url);
-
-      if (type === 'single') {
-        setSingleTasks(singleTasks.filter((task) => task._id !== id));
-      } else {
-        setRecurringTasks(recurringTasks.filter((task) => task._id !== id));
-      }
-
-      alert('Task deleted successfully.');
-    } catch (error) {
-      console.error('Error deleting task:', error);
-      alert('Failed to delete task. Please try again.');
-    }
-  };
-
   const handleTaskCheck = async (id) => {
     try {
-      const updatedTasks = singleTasks.map((task) => {
-        if (task._id === id) {
-          return { ...task, completed: !task.completed };
-        }
-        return task;
-      });
-      setSingleTasks(updatedTasks);
-
-      const taskToUpdate = updatedTasks.find((t) => t._id === id);
-      await axiosInstance.put(`/task/tasks/${id}`, taskToUpdate);
+      dispatch(toggleTaskCompletion(id));
+      const task = singleTasks.find(t => t._id === id);
+      if (task) {
+        await dispatch(updateSingleTask({ ...task, completed: !task.completed })).unwrap();
+      }
     } catch (error) {
       console.error('Failed to update task completion:', error);
     }
@@ -118,16 +62,11 @@ const HomePage = () => {
 
   const handleRecurringCheck = async (id) => {
     try {
-      const updatedTasks = recurringTasks.map((task) => {
-        if (task._id === id) {
-          return { ...task, completed: !task.completed };
-        }
-        return task;
-      });
-      setRecurringTasks(updatedTasks);
-
-      const taskToUpdate = updatedTasks.find((t) => t._id === id);
-      await axiosInstance.put(`/recurringTask/recurring-task/${id}`, taskToUpdate);
+      dispatch(toggleRecurringTaskCompletion(id));
+      const task = recurringTasks.find(t => t._id=== id);
+      if(task){
+        dispatch(updateRecurringTask({ ...task, completed: !task.completed }));
+      }
     } catch (error){
       console.error('Failed to update task completion:', error);
     }
@@ -165,23 +104,6 @@ const HomePage = () => {
           </div>
         ) : (
           <>
-            {/* {singleTasks.length > 0 && (
-              <div>
-                <h2 className={styles.singleTaskH2}>Single Tasks</h2>
-                <div className={styles.taskCards}>
-                  {singleTasks.map((task) => (
-                    <div className={styles.taskCard} key={task._id}>
-                      <h4 className={styles.taskTitle}>{task.name}</h4>
-                      <div className={styles.taskActions}>
-                        <FaEye className={styles.viewIcon} onClick={() => handleViewTask(task)} />
-                        <FaEdit className={styles.editIcon} onClick={() => openEditModal(task, 'single')} />
-                        <FaTrashAlt className={styles.deleteIcon} onClick={() => handleDelete(task._id, 'single')} />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )} */}
             {singleTasks.length > 0 &&
               singleTasks.map((task) => (
                 <div key={task._id} className={styles.taskItem}>
@@ -203,24 +125,6 @@ const HomePage = () => {
               ))
             }
 
-
-            {/* {recurringTasks.length > 0 && (
-              <div className={styles.recurringTaskContainer}>
-                <h2 className={styles.recurringTaskH2}>Recurring Tasks</h2>
-                <div className={styles.taskCards}>
-                  {recurringTasks.map((task) => (
-                    <div className={styles.taskCard} key={task._id}>
-                      <h4 className={styles.taskTitle}>{task.name}</h4>
-                      <div className={styles.taskActions}>
-                        <FaEye className={styles.viewIcon} onClick={() => handleViewTask(task)} />
-                        <FaEdit className={styles.editIcon} onClick={() => openEditModal(task, 'recurring')} />
-                        <FaTrashAlt className={styles.deleteIcon} onClick={() => handleDelete(task._id, 'recurring')} />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )} */}
             { recurringTasks.length > 0 && (
               recurringTasks.map((task) => (
                 <div key={task._id} className={styles.taskItem}>
@@ -251,47 +155,6 @@ const HomePage = () => {
       {isModalOpen && (
         <ChooseTaskModal closeModal={closeModal}/>
       )}
-
-      {editModal.isOpen && (
-        <div className={`${styles.modalEditTask} ${styles.show}`}>
-          <div className={styles.modalContentEdit}>
-            <button className={styles.closeBtnEdit} onClick={handleEditModalClose}>X</button>
-            <h3 className={styles.modalHeader}>Edit Task</h3>
-            <form className={styles.editTaskForm}>
-              <label className={styles.editTaskLabel}>
-                Task Name:
-                <input
-                  className={styles.editTaskInput}
-                  type="text"
-                  value={editModal.task.name}
-                  onChange={(e) =>
-                    setEditModal({ ...editModal, task: { ...editModal.task, name: e.target.value } })
-                  }
-                />
-              </label>
-              <label className={styles.editTaskLabel}>
-                Task Description:
-                <textarea
-                  className={styles.editTaskTextarea}
-                  value={editModal.task.description || ''}
-                  onChange={(e) =>
-                    setEditModal({ ...editModal, task: { ...editModal.task, description: e.target.value } })
-                  }
-                />
-              </label>
-              <button type="button" className={styles.editTaskBtn} onClick={saveTaskChanges}>
-                Save Changes
-              </button>
-            </form>
-          </div>
-        </div>
-      )}
-
-      {
-        isRatePage && (
-          <Rate></Rate>
-        )
-      }
     </div>
   );
 };
