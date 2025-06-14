3dbce2fcfcc60c84046c8c0e1c4a56e2a9a89c87
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-14 22:53:34 +0530

    adding store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import taskReducer from "@/slices/taskSlice";
+import recurringtaskReducer from "@/slices/recurringtaskSlice";
+import userReducer from "@/slices/userSlice";
 
 const rootReducer = combineReducers({
-    tasks : taskReducer 
+    tasks : taskReducer,
+    recurringtasks : recurringtaskReducer,
+    user : userReducer
 })
 
 export default rootReducer;
