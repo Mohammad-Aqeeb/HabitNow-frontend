ef62c457230703d8f94773eca00879a6c680e5be
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-14 20:49:26 +0530

    change in sidenav component

--- src/components/Sidenav/Sidenav.jsx
+++ src/components/Sidenav/Sidenav.jsx
@@ -8,7 +8,7 @@ import styles from "@/styles/Sidenav.module.css";
 
 const Sidenav = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const hiddenToggleRoutes = ["/login", "/rate", "/contact", "/premium", "/timer", "/categories","/setting","/setting"];
+  const hiddenToggleRoutes = ["/login", "/rate", "/contact", "/premium", "/timer", "/categories","/setting"];
   const pathname = usePathname();
   const navRef = useRef(null);
   const btnRef = useRef(null);
