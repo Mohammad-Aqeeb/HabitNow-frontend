82b91f04acf30df3c5975e88f5e265489f9416c7
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-18 21:43:44 +0530

    change in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,31 +6,19 @@ import ProtectedRoute from '@/components/ProtectedRoute';
 
 const HomePage = dynamic(() => import('../components/Home/HomePage'), {
   ssr: false,
+  loading: () => <div>Loading...</div>
 });
 
-const pageVariants = {
-  initial: { opacity: 0, y: 30 },
-  animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-  exit: { opacity: 0, y: -30, transition: { duration: 0.5 } },
-};
+// const pageVariants = {
+//   initial: { opacity: 0, y: 30 },
+//   animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+//   exit: { opacity: 0, y: -30, transition: { duration: 0.5 } },
+// };
 
 export default function Home() {
   return (
-    <motion.div
-      variants={pageVariants}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      style={{
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
       <ProtectedRoute>
         <HomePage />
       </ProtectedRoute>
-    </motion.div>
   );
 }
