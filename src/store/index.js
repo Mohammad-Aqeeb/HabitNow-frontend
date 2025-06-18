6681e38cb6bc44263a112bbf23b15dbc8f920f51
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-18 21:47:53 +0530

    change in store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from "@reduxjs/toolkit";
 import taskReducer from "@/slices/taskSlice";
 import recurringtaskReducer from "@/slices/recurringtaskSlice";
 import userReducer from "@/slices/userSlice";
+import habitReducer from "@/slices/habitSlice";
+import categoryReducer from "@/slices/categorySlice"
 
 const rootReducer = combineReducers({
     tasks : taskReducer,
     recurringtasks : recurringtaskReducer,
+    habits : habitReducer,
+    categories : categoryReducer,
     user : userReducer
 })
 
