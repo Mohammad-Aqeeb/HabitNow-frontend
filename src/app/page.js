4c3c7460d7953d9b8290d9b90c3c93ff55dc040b
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-06-19 12:18:32 +0530

    change in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { motion } from 'framer-motion';
 import ProtectedRoute from '@/components/ProtectedRoute';
 
 const HomePage = dynamic(() => import('../components/Home/HomePage'), {
